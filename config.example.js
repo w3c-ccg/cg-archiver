f4be0222bd7a7025d7f5d4f1fee9b933fee6179f
Manu Sporny <msporny@digitalbazaar.com>
2025-03-10 20:48:58 -0400

    Update config.example.js with latest fields.

--- config.example.js
+++ config.example.js
@@ -1,8 +1,15 @@
 export default {
   community: 'w3c-ccg',
   logDir: './logs',
+  archiveUrl: 'https://meet.w3c-ccg.org/archives/',
   meetings: [{
-    name: 'ccg-test',
-    googleMeetId: 'vnd-kwiq-fwy'
-  }]
+    name: 'VC API',
+    email: 'test@email.example',
+    googleMeetId: 'zyf-jnvc-qac'
+  }, {
+    name: 'Data Integrity',
+    email: 'test@email.example',
+    googleMeetId: 'vep-qjhe-ofm'
+  }],
+  geminiApiKey: 'GEMINI-API-KEY-GOES-HERE'
 }
